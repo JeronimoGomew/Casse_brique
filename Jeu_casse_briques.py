5b1a015d1f5cac6b7603f810b154ea83adcefacd
Jeronimo Gomez <jeronimo.gomez-duque@cpe.fr>
2025-10-07 09:17:31 +0200

    button quiter

--- Jeu_casse_briques.py
+++ Jeu_casse_briques.py
@@ -9,9 +9,14 @@ import tkinter as tk
 
 fenetre = tk.Tk()
 fenetre.title("Casse Briques")
+fenetre.geometry("500x400")
 
 canvas = tk.Canvas(fenetre, width=500, height=400, bg="black")
 canvas.pack()
 
-button_quitter = tk.Button(fenetre,text="quitter", command=fenetre.destroy)
+button_quitter = tk.Button(canvas,text="quitter", command=fenetre.destroy, bg="red", fg="white")
+button_quitter.pack()
+
+fenetre.mainloop()
+
 
