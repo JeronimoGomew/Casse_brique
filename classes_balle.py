6791077cecf4134585593f79e6ca2551949c76b1
Jeronimo Gomez <jeronimo.gomez-duque@cpe.fr>
2025-10-07 11:31:02 +0200

    renomage et creation dún fichier par classe

--- classes_balle.py
+++ classes_balle.py
@@ -47,17 +47,3 @@ class balle:
 
 
 
-class plateforme:
-    def __init__(self,canvas,fenetre,x,y,largeur,hauteur,couleur):
-        self.__canvas=canvas
-        self.__fenetre=fenetre
-        self.__x=x
-        self.__y=y
-        self.__largeur=largeur
-        self.__hauteur=hauteur
-        self.__couleur=couleur
-
-        self.__plateforme=self.__canvas.create_rectangle(self.__x,self.__y,self.__x+self.__largeur,self.__y+self.__hauteur,fill=self.__couleur)
-        self.__canvas.bind_all("<KeyPress-Left>",self.gauche)
-        self.__canvas.bind_all("<KeyPress-Right>",self.droite)
-        self.__canvas_width=self.__canvas.winfo_width()
