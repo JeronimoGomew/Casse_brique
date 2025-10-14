3d7137d2f99d29a888939401d358c911aea0eb33
Jeronimo Gomez <jeronimo.gomez-duque@cpe.fr>
2025-10-14 08:54:21 +0200

    collision plateforme

--- classe_plateforme.py
+++ classe_plateforme.py
@@ -17,19 +17,50 @@ class plateforme:
         self.__couleur=couleur
 
         self.__plateforme=self.__canvas.create_rectangle(self.__x,self.__y,self.__x+self.__largeur,self.__y+self.__hauteur,fill=self.__couleur)
-        self.__canvas.bind('<Key>',self.bouger_plateforme)
+        self.__canvas.focus_set()
+        self.__canvas.bind('<Left>',self.bouger_plateforme)
+        self.__canvas.bind('<Right>',self.bouger_plateforme)
+        
         self.__canvas_width=self.__canvas.winfo_width()
 
+
+    def getx(self):
+        return self.__x
+    def gety(self):
+        return self.__y
+    def getlargeur(self):
+        return self.__largeur
+    def gethauteur(self):
+        return self.__hauteur
+
+    
+    
+    
+    
     def bouger_plateforme(self,event):
         touche=event.keysym
+        print(touche)
 
-        if touche == 'Right':
-            self.__x += 20
-        elif touche == 'Left':
-            self.__x -= 20
+        if self.__x + self.__largeur +10 > 1000:
+            if touche == 'Right':
+                pass
+            elif touche == 'Left':
+                self.__x -= 20
         
-        self.__canvas.coords(self.__plateforme, self.__x, self.__y, self.__x+ self.__largeur, self.__y + self.__hauteur, fill=self.__couleur)
-
+        elif self.__x -10 < 0:
+            if touche == 'Right':
+                self.__x += 20
+            elif touche == 'Left':
+                pass
+        
+        else: 
+            if touche == 'Right':
+                self.__x += 20
+            elif touche == 'Left':
+                self.__x -= 20
+            
+        self.__canvas.coords(self.__plateforme, self.__x, self.__y, self.__x+ self.__largeur, self.__y + self.__hauteur)
+        
 
         
 
